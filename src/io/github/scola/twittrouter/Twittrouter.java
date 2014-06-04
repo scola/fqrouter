4e18e603c581be6e0eca98a80228ebb43e2c662f
scola <friend_21@yahoo.cn>
2014-06-04 19:33:09 +0800

    bug fix

--- src/io/github/scola/twittrouter/Twittrouter.java
+++ src/io/github/scola/twittrouter/Twittrouter.java
@@ -137,7 +137,7 @@ public class Twittrouter extends Activity implements
         }
         
         try {
-            Thread.sleep(100);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
